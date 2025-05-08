68869425599924f7421e312d29afe3c8e668915f
miranda-cortez1 <mirandacortez@ucmerced.edu>
2025-05-08 12:31:10 -0700

    Update Application.h

--- Application.h
+++ Application.h
@@ -17,42 +17,36 @@ class Application : public bobcat::Application_ {
 
     bobcat::TextBox* titleTextbox;
 
-    //variables
     std::string currentScreen;
     int roundNumber;
+    bool gameOver = false;
 
-    //shared screen variables
     bobcat::Button* returnToMainMenuButton;
+    bobcat::Button* clearStatsButton;
 
-    //main menu screen variables
     bobcat::Button* playGameButton;
     bobcat::Button* instructionsButton;
     bobcat::Button* statsButton;
     bobcat::Button* quitGameButton;
 
-    //instructions screen variables
     bobcat::TextBox* instructionsTextBox;
 
-    //statistics screen variables
     bobcat::TextBox* statisticsRoundTextbox;
     bobcat::TextBox* statisticsOutcomesTextbox;
     bobcat::Button* playAgainButton;
 
-    //play game screen variables
     bobcat::Button* giveUpButton;
     bobcat::TextBox* gameStateTextbox;
     bobcat::TextBox* spinningLetterTextbox;
     bobcat::Button* slotButtons[6];
 
-    //game logic variables
     std::vector<char> selectedLetters;
     char placedLetters[6];
     bool slotUsed[6];
     char currentLetter;
-    bool isSpinning; // whether letter is spinning
+    bool isSpinning;
     int spinCount = 0;
 
-    //write data to file
     void writeDataToFile(int roundNumber, std::string outcome) {
         std::ofstream file("statistics.csv", std::ios::app);
         if (file.is_open()) {
@@ -65,7 +59,6 @@ class Application : public bobcat::Application_ {
         app->spinStep();
     }
 
-    //load last round number
     void loadLastRoundNumber() {
         std::ifstream file("statistics.csv");
         std::string line, lastLine;
@@ -86,7 +79,12 @@ class Application : public bobcat::Application_ {
         }
     }
 
-    //read stats from file and only displays most recent 10 rounds
+    void clearStatsFile() {
+        std::ofstream file("statistics.csv", std::ios::trunc);
+        file.close();
+        roundNumber = 0;
+    }
+
     void readStatsFromFile(){
         std::ifstream file("statistics.csv");
         std::string line;
@@ -109,8 +107,6 @@ class Application : public bobcat::Application_ {
         }
     }
 
-
-    //hide all visual components
     void hideAll() {
         playGameButton->hide();
         instructionsButton->hide();
@@ -120,6 +116,7 @@ class Application : public bobcat::Application_ {
         quitGameButton->hide();
         instructionsTextBox->hide();
         returnToMainMenuButton->hide();
+        clearStatsButton->hide();
         playAgainButton->hide();
         giveUpButton->hide();
         gameStateTextbox->hide();
@@ -127,7 +124,6 @@ class Application : public bobcat::Application_ {
         for (int i = 0; i < 6; ++i) slotButtons[i]->hide();
     }
 
-    //update game state label
     void updateGameStateLabel() {
         std::string display = "Slots: ";
         for (int i = 0; i < 6; ++i) {
@@ -139,7 +135,6 @@ class Application : public bobcat::Application_ {
         gameStateTextbox->align(FL_ALIGN_CENTER);
     }
 
-    //function to handle rendering of the current screen
     void renderCurrentScreen() {
         hideAll();
         if (currentScreen == "Main Menu") {
@@ -159,14 +154,15 @@ class Application : public bobcat::Application_ {
             statisticsOutcomesTextbox->show();
             playAgainButton->show();
             returnToMainMenuButton->show();
+            clearStatsButton->show();
         } else if (currentScreen == "Game") {
             startNewGame();
         }
     }
 
-    //function to start a new game
     void startNewGame() {
         roundNumber++;
+        gameOver = false;
 
         selectedLetters.clear();
         for (char c = 'A'; c <= 'Z'; ++c) selectedLetters.push_back(c);
@@ -197,7 +193,6 @@ class Application : public bobcat::Application_ {
         Fl::add_timeout(0.2, Application::spinStepCallback, this);
     }
 
-     //checks if the letter can be placed properly, if not then the game ends
     bool canPlaceLetter(char letter){
         for(int i=0; i < 6; i++){
             if(slotUsed[i]) continue;
@@ -209,7 +204,7 @@ class Application : public bobcat::Application_ {
                     break;
                 }
             }
-        
+
             char right = '\0';
             for(int j = i + 1; j < 6; j++){
                 if(slotUsed[j]){
@@ -231,18 +226,17 @@ class Application : public bobcat::Application_ {
         app->checkGameOverCannotPlace();
     }
 
-    //checks if letter is placeable
     void checkGameOverCannotPlace(){
-        if(!canPlaceLetter(currentLetter)){
+        if (gameOver) return;
+        if (!canPlaceLetter(currentLetter)) {
+            gameOver = true;
             bobcat::showMessage("Letter has no valid placement! Game Over!");
             writeDataToFile(roundNumber, "Loss");
             currentScreen = "Main Menu";
             renderCurrentScreen();
-            }
         }
+    }
 
-
-    //spin letters without freezing UI
     void spinStep(){
         if(spinCount < 15){
             char temp = 'A' + rand() % 26;
@@ -250,20 +244,17 @@ class Application : public bobcat::Application_ {
             spinningLetterTextbox->label(std::string("Generating:...") + temp);
             spinCount++;
             Fl::add_timeout(0.2, spinStepCallback, this);
-        }else{
+        } else {
             spinningLetterTextbox->label(std::string("Place: ") + currentLetter);
             spinningLetterTextbox->align(FL_ALIGN_CENTER);
             spinningLetterTextbox->labelsize(27);
             isSpinning = false;
-
             Fl::add_timeout(2.5, Application::delayedGameOverCallback, this);
         }
     }
 
-
-    //handle navigation buttons
     void handleNavigationClick(bobcat::Widget* sender) {
-        if (sender == playGameButton|| sender == playAgainButton) {
+        if (sender == playGameButton || sender == playAgainButton) {
             currentScreen = "Game";
             renderCurrentScreen();
         } else if (sender == instructionsButton) {
@@ -275,14 +266,16 @@ class Application : public bobcat::Application_ {
         } else if (sender == returnToMainMenuButton) {
             currentScreen = "Main Menu";
             renderCurrentScreen();
+        } else if (sender == clearStatsButton) {
+            clearStatsFile();
+            readStatsFromFile();
         } else if (sender == quitGameButton) {
             window->hide();
         }
     }
 
-    //handle slot clicks
     void handleSlotClick(bobcat::Widget* sender) {
-        if (isSpinning) return; // don't allow slot choice while spinning
+        if (isSpinning || gameOver) return;
 
         for (int i = 0; i < 6; ++i) {
             if (sender == slotButtons[i] && !slotUsed[i]) {
@@ -292,6 +285,7 @@ class Application : public bobcat::Application_ {
                 updateGameStateLabel();
 
                 if (selectedLetters.empty()) {
+                    gameOver = true;
                     std::vector<char> finalLetters(placedLetters, placedLetters + 6);
                     if (std::is_sorted(finalLetters.begin(), finalLetters.end())) {
                         bobcat::showMessage("You Win!");
@@ -317,10 +311,10 @@ class Application : public bobcat::Application_ {
         }
     }
 
-
-    //handle give up
     void handleGameClick(bobcat::Widget* sender) {
+        if (gameOver) return;
         if (sender == giveUpButton) {
+            gameOver = true;
             bobcat::showMessage("You gave up. Game Over.");
             writeDataToFile(roundNumber, "Loss");
             currentScreen = "Main Menu";
@@ -341,7 +335,6 @@ public:
         titleTextbox->align(FL_ALIGN_CENTER);
         titleTextbox->labelsize(30);
 
-        // main menu screen
         playGameButton = new bobcat::Button(75, 95, 350, 40, "Play Game");
         instructionsButton = new bobcat::Button(75, 145, 350, 40, "View Instructions");
         statsButton = new bobcat::Button(75, 195, 350, 40, "View Statistics");
@@ -351,7 +344,6 @@ public:
         ON_CLICK(statsButton, Application::handleNavigationClick);
         ON_CLICK(quitGameButton, Application::handleNavigationClick);
 
-        // instructions screen
         instructionsTextBox = new bobcat::TextBox(50, 75, 400, 200,
             "- You will be given 6 random letters.\n\n"
             "- Place each letter into one of the 6 slots.\n\n"
@@ -359,7 +351,6 @@ public:
             "- Try to order them alphabetically!");
         instructionsTextBox->align(FL_ALIGN_CENTER);
 
-        // statistics screen
         statisticsRoundTextbox = new bobcat::TextBox(50, 75, 100, 250, "Round");
         statisticsOutcomesTextbox = new bobcat::TextBox(250, 75, 100, 250, "Outcome");
         statisticsRoundTextbox->align(FL_ALIGN_TOP_LEFT);
@@ -367,10 +358,11 @@ public:
 
         playAgainButton = new bobcat::Button(75, 300, 350, 30, "Play Again");
         ON_CLICK(playAgainButton, Application::handleNavigationClick);
-        
 
-        returnToMainMenuButton = new bobcat::Button(75, 350, 350, 30, "Return to Main Menu");
+        returnToMainMenuButton = new bobcat::Button(75, 350, 150, 30, "Return to Main Menu");
+        clearStatsButton = new bobcat::Button(250, 350, 150, 30, "Clear Stats");
         ON_CLICK(returnToMainMenuButton, Application::handleNavigationClick);
+        ON_CLICK(clearStatsButton, Application::handleNavigationClick);
 
         giveUpButton = new bobcat::Button(75, 350, 350, 30, "Give Up");
         ON_CLICK(giveUpButton, Application::handleGameClick);
